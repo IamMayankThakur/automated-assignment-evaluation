4c63c818fd22d4b67e4331b98af0082d96caa291
Mayank Thakur <thakurmayank88@gmail.com>
2020-01-18 20:12:15 +0530

    Not completed models

--- evalmgr/evalmgr/settings.py
+++ evalmgr/evalmgr/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'i&^l7d&4&$mdbf5gy+atcj#njzfuh&93d0@gwnp@%bzeqo3w5c'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'evalmgr.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -101,20 +97,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- evalmgr/facultymgr/models.py
+++ evalmgr/facultymgr/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Evaluation(models.Model):
+    name = models.CharField(max_length=128, unique=True, blank=False)
+    description = models.TextField(blank=True)
+    created_on = models.DateTimeField(default=timezone.now)
+    begins_on = models.DateTimeField(default=timezone.now)
+    ends_on = models.DateTimeField(blank=False)
