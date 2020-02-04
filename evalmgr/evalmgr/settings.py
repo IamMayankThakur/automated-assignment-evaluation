ee30631ede0691803fb7ca869e82335e906c8c95
Mayank Thakur <thakurmayank88@gmail.com>
2020-02-04 18:46:57 +0530

    On celery

--- evalmgr/evalmgr/settings.py
+++ evalmgr/evalmgr/settings.py
@@ -119,9 +119,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+CELERY_TIMEZONE = TIME_ZONE
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_SERIALIZER = 'json'
