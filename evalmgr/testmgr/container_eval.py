504905b962e687222801a3b055c9e299364e6489
Mayank Thakur <thakurmayank88@gmail.com>
2020-02-28 18:50:59 +0530

    Big Fix

--- evalmgr/testmgr/container_eval.py
+++ evalmgr/testmgr/container_eval.py
@@ -33,9 +33,10 @@ def do_container_eval_cc(*args, **kwargs):
             message += "Rides container not running. "
         stdin, stdout, stderr = ssh.exec_command("sudo docker ps --format '{{.ID}} {{.Image}}' | grep users:latest | cut -d ' ' -f 1")
         con_id = stdout.read().decode()
-        stdin, stdout, stderr = ssh.exec_command("sudo docker exec "+con_id+" env printenv grep TEAM_NAME")
+        stdin, stdout, stderr = ssh.exec_command("sudo docker exec "+con_id+" env printenv TEAM_NAME")
         team_name = stdout.read().decode()
-        print("sudo docker exec "+con_id+" env printenv grep TEAM_NAME")
+        print("CONID", con_id)
+        print("sudo docker exec "+con_id+" env printenv TEAM_NAME")
         print("TEAMNAME", team_name)
         if sub.team.team_name in team_name:
             message += "Team name set in users container. "
@@ -46,7 +47,8 @@ def do_container_eval_cc(*args, **kwargs):
         con_id = stdout.read().decode()
         stdin, stdout, stderr = ssh.exec_command("sudo docker exec " + con_id + " env printenv TEAM_NAME")
         team_name = stdout.read().decode()
-        print("sudo docker exec " + con_id + " env printenv grep TEAM_NAME")
+        print("CONID", con_id)
+        print("sudo docker exec " + con_id + " env printenv TEAM_NAME")
         print("TEAMNAME", team_name)
         if sub.team.team_name in team_name:
             message += "Team name set in rides container. "
