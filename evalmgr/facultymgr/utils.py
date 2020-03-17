b03d724a40554af7127e85e9de426cbc539b8b13
Mayank Thakur <thakurmayank88@gmail.com>
2020-03-17 16:37:39 +0530

    WIP: Consolidated Marks Feature. Update facultymgr/utils.py

--- evalmgr/facultymgr/utils.py
+++ evalmgr/facultymgr/utils.py
@@ -1,11 +1,15 @@
 import configparser
-from .models import Evaluation, FacultyProfile
-from testmgr.api_eval import setup_api_eval
-from studentmgr.models import Team
+import csv
+
 import pandas as pd
-from testmgr.code_eval import setup_code_eval
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Max
 from django.http import HttpResponse
+
+from studentmgr.models import Team, Submission
+from testmgr.api_eval import setup_api_eval
+from testmgr.code_eval import setup_code_eval
+from .models import Evaluation, FacultyProfile
 
 
 def create_evaluation(**kwargs):
@@ -69,3 +73,56 @@ def create_evaluation_code_eval(**kwargs):
         setup_code_eval.delay(eval_id=eval_id)
     except ObjectDoesNotExist:
         return HttpResponse("FacultyProfile object not found")
+
+
+# def get_max_marks():
+#     a1 = list(
+#         Submission.objects.filter(evaluation=2)
+#         .values("team__team_name")
+#         .annotate(Max("marks"))
+#     )
+#     a2 = list(
+#         Submission.objects.filter(evaluation=4)
+#         .values("team__team_name")
+#         .annotate(Max("marks"))
+#     )
+
+#     all_rows = []
+#     team_usn_mapping = {}
+#     csv_file = csv.reader(open("../../teamslist.csv", "r"), delimiter=",")
+#     for row in csv_file:
+#         print(row)
+#         team_usn_mapping[row[24]] = []
+#     csv_file = csv.reader(open("new_bd_resp.csv", "r"), delimiter=",")
+#     for row in csv_file:
+#         team_usn_mapping[row[24]].append([row[1], row[2], row[3]])
+#         team_usn_mapping[row[24]].append([row[5], row[6], row[7]])
+#         if row[10] != "":
+#             team_usn_mapping[row[24]].append([row[10], row[11], row[12]])
+#         if row[15] != "":
+#             team_usn_mapping[row[24]].append([row[15], row[16], row[17]])
+
+#     for i in range(len(s1)):
+#         try:
+#             usn_list = team_usn_mapping[s1[i]["team__team_name"]]
+#             print("team exists")
+#         except Exception as e:
+#             print("team does not exist")
+#             continue
+#         for usn in usn_list:
+#             print(usn)
+#             all_rows.append(
+#                 [
+#                     usn[0],
+#                     usn[2],
+#                     usn[1],
+#                     s1[i]["score_1__max"],
+#                     #  s2[i]['score_2__max'], s3[i]['score_3__max'],
+#                     s2[i]["score_2__max"],
+#                     s1[i]["team__team_name"],
+#                 ]
+#             )
+#     with open("marks_a3.csv", "w") as csvFile:
+#         writer = csv.writer(csvFile)
+#         writer.writerows(all_rows)
+#     csvFile.close()
