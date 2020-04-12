d478242879b51ff5263c61db3eac52b5a3786304
Mayank Thakur <mthakur@akamai.com>
2020-04-12 19:53:22 +0530

    UI Fixed

--- evalmgr/facultymgr/urls.py
+++ evalmgr/facultymgr/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "facultymgr"
 
 urlpatterns = [
-    path("upload_config", views.ConfigUpload.as_view()),
-    path("upload_config2", views.ConfigUploadCodeEval.as_view()),
+    path("upload_config", views.ConfigUpload.as_view(), name='create_api_eval'),
+    path("upload_config2", views.ConfigUploadCodeEval.as_view(), name='create_code_eval'),
     path("view_submissions", views.SubmissionView.as_view(), name="view_submission"),
 ]
